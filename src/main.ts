93c568f95e47b5152c7398baca9e28a9a09b5996
chee <yay@chee.party>
2026-07-21 14:15:35 +0200

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import setup, {
+	hideLoadingAnimation,
+	showErrorScreen,
+	showLoadingAnimation,
+} from "@inkandswitch/patchwork"
+
+const DEFAULT_PACKAGE_LIST =
+	"https://base.pkg.patchwork.inkandswitch.com/modules.json"
+
+const packageListURL = (
+	import.meta.env.PATCHWORK_SYSTEM_PACKAGE_LIST_URL ||
+	import.meta.env.VITE_DEFAULT_MODULES ||
+	DEFAULT_PACKAGE_LIST
+)
+	.split(",")
+	.map(source => source.trim())
+	.filter(Boolean)
+
+showLoadingAnimation()
+
+window.patchwork = await setup({
+	packageListURL,
+	accountKey: "tinyPatchworkAccountUrl",
+	name: "patchwork",
+}).catch(error => {
+	showErrorScreen(error, {contact: "chee@inkandswitch.com"})
+	throw error
+})
+
+hideLoadingAnimation()
